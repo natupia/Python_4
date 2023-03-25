935326b28b19d794c65da84a44127968cf29c132
Natalia <nataliahola@yandex.ru>
2023-03-25 15:00:37 +0300

    first commit

---
+++ Python Lecture1/main.py
@@ -0,0 +1,16 @@
+
+# print('введите 1-ое число:')
+# a = input()
+# print('введите 2-ое число:')
+# b = input()
+# a = int(a)
+# b = int(b)
+# print(a, '+', b, '=', a + b)
+
+# if k % p // n:
+#     print('yes') 
+# elif k % p // m:
+#     print('yes')
+
+print (6//2)
+print (5%3)
